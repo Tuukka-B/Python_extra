8fe11e9419dec571a30c61bd9081fa6629f90c6e
Tuukka-B <60816421+Tuukka-B@users.noreply.github.com>
2020-02-12 18:36:17 +0200

    Minor changes / code cleaning.
    Changed default values of certain argparse arguments to cleaner versions.

--- M2296_Assignment_05/venv/M2296_assignment05.py
+++ M2296_Assignment_05/venv/M2296_assignment05.py
@@ -1,13 +1,12 @@
 import argparse
 import GFTP
-import time
 
 parser = argparse.ArgumentParser(description='A front-end for establishing and serving connections with GFTP-protocol.')
 subparser = parser.add_subparsers()
 parser_server = subparser.add_parser('server')
 parser_server.add_argument('hostname', type=str, help='an argument for establishing a socket name or ip-address')
 parser_server.add_argument('port', type=int, help='an argument for establishing a port number (int)')
-parser_server.add_argument('-d', "--working_directory", type=str, help='the directory the server serves its operations on; if argument is unsupplied this command defaults to ./server-files/', default= "./server-files/")
+parser_server.add_argument('-d', "--working_directory", type=str, help='the directory the server serves its operations on; if argument is unsupplied this command defaults to ./server-files/', default= "server-files")
 parser_client = subparser.add_parser('client')
 parser_client.add_argument("remote_ip", type=str, help="If you are connecting as a client, input a hostname or ip-address to connect to")
 parser_client.add_argument("remote_port", type=int, help="If you are connecting as a client, input a port to connect to. Must be supplied as an integer")
@@ -15,10 +14,10 @@ clientsub = parser_client.add_subparsers()
 parser.list = clientsub.add_parser("list")
 parser.download = clientsub.add_parser("download")
 parser.download.add_argument("target_file", type=str, help="The name of the file you want to download from the server")
-parser.download.add_argument("-d", "--destination_folder", type=str, help="the folder you want to save the file to; default is current folder", default="./")
+parser.download.add_argument("-d", "--destination_folder", type=str, help="the folder you want to save the file to; default is current folder", default=".")
 parser.upload = clientsub.add_parser("upload")
 parser.upload.add_argument("filepath", type=str, help="the name of the file you want to send to the server; searches current directory for match if only name of the file is specified, otherwise tries to send the file with exact file destination")
-parser.upload.add_argument("-s", "--source_folder", type=str, help="the folder you want to send the file from; default is current folder", default="./")
+parser.upload.add_argument("-s", "--source_folder", type=str, help="the folder you want to send the file from; default is current folder", default=".")
 args = parser.parse_args()
 a = None
 # GFTP takes parameters in this order: mode, ip, port, fileloc (=keyword argument)
